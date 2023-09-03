09967b5710f03c9bb2fbecb4852c094e0851acea
Mateusz <mateuszmstawski@gmail.com>
2023-09-03 22:15:49 +0200

    Feat: adding pdf generating ability

---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="title-text" style="color: black; width: fit-content;">
+        Page not found
+    </div>
+    <div style="margin-top: 30px; text-align: center;">
+        <a href="/creator"> 
+            Redirect to main page
+        </a>
+    </div>
+</template>
